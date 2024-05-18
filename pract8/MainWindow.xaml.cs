0789c48d3a5a384f9e1a52c61b168188c9964751
Sapgor <vasyuckov.n@yandex.ru>
2024-05-18 23:05:44 +0300

    pract8

---
+++ pract8/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MVVMLibrary;
+using SerializationLibrary;
+
+namespace SimpleCalculator
+{
+    public class CalculatorViewModel : BindableBase
+    {
+        private int _a;
+        public int a
+        {
+            get { return _a; }
+            set
+            {
+                a = value;
+                OnPropertyChanged(nameof(a));
+            }
+        }
+
+        private int _b;
+        public int b
+        {
+            get { return _b; }
+            set
+            {
+                b = value;
+                OnPropertyChanged(nameof(b));
+            }
+        }
+
+        private int _result;
+        public int Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand minusCommand { get; set; }
+
+        public CalculatorViewModel()
+        {
+            AddCommand = new BindableCommand(Add, CanCalculate);
+            minusCommand = new BindableCommand(minus, CanCalculate);
+        } 
+
+        private bool CanCalculate()
+        {
+            return a != 0 && b != 0;
+        }
+
+        private void Add()
+        {
+            Result = a + b;
+        }
+
+        private void minus()
+        {
+            Result = a - b;
+        }
+    }
+}
---
+++ pract8/mvvm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MVVMLibrary
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class BindableCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public BindableCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
